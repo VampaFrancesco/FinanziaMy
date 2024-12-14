b4011d3bf5a942f7a42112aaa9d3d0f6537c6053
miriamdevincentiis <midevi03is@gmail.com>
2024-12-14 21:14:33 +0100

    Interfaccia pagina iniziale
    Implementazione pagina iniziale landing.fxml
    Implementazione LandingController.java

--- src/main/java/it/univaq/cdvd/Runner.java
+++ src/main/java/it/univaq/cdvd/Runner.java
@@ -17,7 +17,7 @@ public class Runner extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         try {
-            Parent root = FXMLLoader.load((Objects.requireNonNull(getClass().getResource("/view/auth.fxml"))));
+            Parent root = FXMLLoader.load((Objects.requireNonNull(getClass().getResource("/view/landing.fxml"))));
             Scene scene = new Scene(root);
             Image icon = new Image(Objects.requireNonNull(getClass().getResource("/img/logoApp.jpg")).toExternalForm());
             stage.getIcons().add(icon);
