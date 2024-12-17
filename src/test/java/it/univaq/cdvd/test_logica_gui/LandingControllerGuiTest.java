96f0e60c61129f3db2b1c7a875bd254e67f947e5
miriamdevincentiis <midevi03is@gmail.com>
2024-12-17 12:59:05 +0100

    Test LandingControllerGuiTest.java
    Implementazione del test per verificare il collegamento tra la pagina iniziale e la pagina di login e di registrazione

---
+++ src/test/java/it/univaq/cdvd/test_logica_gui/LandingControllerGuiTest.java
@@ -0,0 +1,67 @@
+package it.univaq.cdvd.test_logica_gui;
+
+
+import it.univaq.cdvd.controller.LandingController;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.Test;
+import org.testfx.api.FxToolkit;
+import org.testfx.framework.junit5.ApplicationTest;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LandingControllerGuiTest extends ApplicationTest {
+
+    LandingController controller = new LandingController();
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/landing.fxml"));
+        Parent root = loader.load();
+        controller = loader.getController();
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+    @Test
+    public void testInitialize() {
+        assertNotNull(controller);
+    }
+
+
+    @Test
+    void testLoginButtonDisplaysLoginPage() throws Exception {
+
+        clickOn(controller.loginInit);
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/login.fxml"));
+        Parent expectedRoot = loader.load();
+
+        // Get the current scene root
+        Parent actualRoot = FxToolkit.toolkitContext().getRegisteredStage().getScene().getRoot();
+
+        // Compare the roots
+        assertEquals(expectedRoot.getId(), actualRoot.getId(), "The auth.fxml page should be displayed.");
+    }
+
+    @Test
+    void testSignUpButtonDisplaysRegistrationPage() throws Exception {
+        // Click the annulButton
+        clickOn(controller.signupInit);
+
+        // Load the auth.fxml to compare
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/registrazione.fxml"));
+        Parent expectedRoot = loader.load();
+
+        // Get the current scene root
+        Parent actualRoot = FxToolkit.toolkitContext().getRegisteredStage().getScene().getRoot();
+
+        // Compare the roots
+        assertEquals(expectedRoot.getId(), actualRoot.getId(), "The landing.fxml page should be displayed.");
+    }
+}
+
