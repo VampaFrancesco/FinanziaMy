ef6cab4ec3b41cce821cdd312600fc44127cc945
miriamdevincentiis <midevi03is@gmail.com>
2024-12-17 11:11:49 +0100

    Collegare la pagina iniziale e home
    Implementazione del codice per collegare la pagine iniziale  e pagine home

--- src/main/java/it/univaq/cdvd/controller/HomeController.java
+++ src/main/java/it/univaq/cdvd/controller/HomeController.java
@@ -1,8 +1,18 @@
 package it.univaq.cdvd.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.TableView;
+import javafx.scene.layout.Region;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class HomeController {
 
     @FXML public MenuBar menu = new MenuBar();
@@ -28,5 +38,55 @@ public class HomeController {
     @FXML public MenuItem graficoLinee = new MenuItem();
     @FXML public MenuItem creaReport = new MenuItem();
     @FXML public MenuItem info = new MenuItem();
+
+
+    @FXML
+    public void nuovaTransazioneOnAction(ActionEvent event) {
+
+        nuovaTransazione.setOnAction(evento -> {
+            try {
+                apriDialog();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+      /*  try {
+            // Carica il file auth.fxml
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/landing.fxml"));
+            Parent root = loader.load();
+
+            // Ottieni la finestra corrente e imposta la nuova scena
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.setTitle("Nuova Transazione");
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+    }
+
+    public void apriDialog() throws IOException {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/landing.fxml"));
+            Parent root = loader.load();
+
+            Dialog<Void> dialog = new Dialog<>();
+            dialog.setTitle("Nuova Transazione");
+            dialog.getDialogPane().setContent(root);
+
+            dialog.getDialogPane().setMinHeight(Region.USE_COMPUTED_SIZE);
+            dialog.getDialogPane().setMinWidth(Region.USE_COMPUTED_SIZE);
+
+            dialog.getDialogPane().getButtonTypes().add(ButtonType.CLOSE);
+
+            dialog.showAndWait();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
 
 }
--- src/main/java/it/univaq/cdvd/controller/LandingController.java
+++ src/main/java/it/univaq/cdvd/controller/LandingController.java
@@ -1,7 +1,15 @@
 package it.univaq.cdvd.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LandingController {
 
@@ -11,4 +19,42 @@ public class LandingController {
     @FXML public Button leggiCategorie = new Button();
     @FXML public Button leggiStatistiche = new Button();
     @FXML public Button leggiReport = new Button();
+
+    @FXML
+    public void landingLoginButtonOnAction(ActionEvent event) {
+
+        try {
+            // Carica il file auth.fxml
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/home.fxml"));
+            Parent root = loader.load();
+
+            // Ottieni la finestra corrente e imposta la nuova scena
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.setTitle("Login");
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @FXML
+    public void landingSingUpButtonOnAction(ActionEvent event) {
+
+        try {
+            // Carica il file auth.fxml
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/home.fxml"));
+            Parent root = loader.load();
+
+            // Ottieni la finestra corrente e imposta la nuova scena
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.setTitle("Registrazione");
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
