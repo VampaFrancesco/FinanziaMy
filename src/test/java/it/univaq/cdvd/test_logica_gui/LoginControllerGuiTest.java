ea0e39e03aab492d29a8e1f2986157f353083177
Francesco <77879644+VampaFrancesco@users.noreply.github.com>
2024-12-17 12:10:36 +0100

    modificato LoginControllerGuiTest

--- src/test/java/it/univaq/cdvd/test_logica_gui/LoginControllerGuiTest.java
+++ src/test/java/it/univaq/cdvd/test_logica_gui/LoginControllerGuiTest.java
@@ -1,6 +1,7 @@
 package it.univaq.cdvd.test_logica_gui;
 
 
+import it.univaq.cdvd.controller.LoginController;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -17,39 +18,30 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class LoginControllerGuiTest extends ApplicationTest {
 
-    @BeforeEach
-    void setup() throws Exception {
-        FxToolkit.registerPrimaryStage(); // Registra lo stage principale
-
-        FxToolkit.setupStage(stage -> {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/login.fxml"));
-            Parent root = null;
-            try {
-                root = loader.load();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-            stage.setScene(new Scene(root));
-            stage.show();
-        });
-    }
+    LoginController controller = new LoginController();
 
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/login.fxml"));
         Parent root = loader.load();
+        controller = loader.getController();
         stage.setScene(new Scene(root));
         stage.show();
+    }
+
+    @Test
+    public void testInitialize() {
+        assertNotNull(controller);
     }
 
 
     @Test
     void testLoginButtonDisplaysOtherPage() throws Exception {
 
-        clickOn("#usernameTextField").write("MarioRossi");
-        clickOn("#passwordPasswordField").write("esempio@gmail.com");
+        clickOn(controller.usernameTextField).write("MarioRossi");
+        clickOn(controller.passwordPasswordField).write("esempio@gmail.com");
 
-        clickOn("#loginButton");
+        clickOn(controller.loginButton);
 
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/home.fxml"));
         Parent expectedRoot = loader.load();
@@ -64,7 +56,7 @@ public class LoginControllerGuiTest extends ApplicationTest {
     @Test
     void testAnnulButtonDisplaysAuthPage() throws Exception {
         // Click the annulButton
-        clickOn("#annulButton");
+        clickOn(controller.annulButton);
 
         // Load the auth.fxml to compare
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/landing.fxml"));
